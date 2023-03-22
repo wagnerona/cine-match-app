3e52bcf6b64e1cb66ba2f8fc3e5e2a8baa08e7bf
Fiona Lai <fitzfinoo@gmail.com>
2023-03-22 20:34:37 +0000

    test

---
+++ src/pages/Home/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <div>
+    <body>
+
+    <img class="logo" src="./logo2.png" />
+    
+    
+  
+    </body>
+
+
+          
+    </div>
+    
+  )
+}
+
+export default Home
