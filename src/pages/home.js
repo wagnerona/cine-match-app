516ef288bfccbfdee0a982edba1997bcc5b0f083
Fiona Lai <fitzfinoo@gmail.com>
2023-03-23 19:24:34 +0000

    test

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './pages/components/Navbar';
+import logoImage from './pages/images/logo2.png';
 
 const Home = () => {
   return (
-   <div>
-    <body>
-
-    <img class="logo" src="./logo2.png" />
-    
-    
-  
-    </body>
-
-
+    <div className="homepage">
+        <Navbar />
+        <div className="logo-background">
+          <img src={logoImage} alt="Logo"/>
+          <button className="center-button">Let's go!</button>
           
-    </div>
-    
-  )
-}
+          
+          
+        </div>
+      </div>
+    );
+  }
+
+
 
 export default Home
