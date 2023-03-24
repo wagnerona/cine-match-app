ae5df8a839bfdbc3921b3e6f2a99ba5636d35fc5
Joanna Booth <118810624+stillawake17@users.noreply.github.com>
2023-03-24 16:11:28 +0000

    added a shortlist function to the right button

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import movies from "../data/movies.json";
-// import { ShortList } from "./Short-list.js";
+
 
 function MovieCard() {
   const [swipeDirection, setSwipeDirection] = useState(null);
   const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
+  const [shortlist, setShortlist] = useState([]);
+
+    
+ 
 
   function handleSwipe(direction) {
-    setSwipeDirection(direction);
-    setTimeout(() => {
-      // reset swipe direction after some time to show the next card
-      setSwipeDirection(null);
-      setCurrentMovieIndex(currentMovieIndex + 1);
-    }, 500); // 500ms delay before showing the next card
+    // setSwipeDirection(direction);
+    // setTimeout(() => {
+    //   // reset swipe direction after some time to show the next card
+    //   setSwipeDirection(null);
+    //   setCurrentMovieIndex(currentMovieIndex + 1);
+    // }, 500); // 500ms delay before showing the next card
   }
+
+  function handleSwipe(direction) {
+    if (direction === "right") {
+      const currentMovie = movies[currentMovieIndex];
+      setShortlist([...shortlist, currentMovie]); // add current movie to shortlist
+    }
+    setCurrentMovieIndex(currentMovieIndex + 1); // show next movie
+  }
+
 
   if (currentMovieIndex >= movies.length) {
     // end of movie list, show some message or redirect to another page
@@ -28,23 +41,21 @@ function MovieCard() {
       <h2>{currentMovie.title}</h2>
       <p>{currentMovie.description}</p>
       <div className="swipe-buttons">
-        <button
-          className="leftButton"
-          disabled={swipeDirection === "left"}
-          onClick={() => handleSwipe("left")}
-        >
-          Swipe left
-        </button>
-        <button
-          className="rightButton"
-          disabled={swipeDirection === "right"}
-          onClick={() => handleSwipe("right")}
-        >
-          Swipe right
-        </button>
+        <button className="leftButton" onClick={() => handleSwipe("left")}>Swipe left</button>
+        <button className="rightButton" onClick={() => handleSwipe("right")}>Swipe right</button>
+      </div>
+      <div className="shortlist">
+        <h3>Shortlist</h3>
+        <ul>
+          {shortlist.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
 }
+
+
 
 export default MovieCard;
